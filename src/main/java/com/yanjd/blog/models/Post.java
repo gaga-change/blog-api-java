656df9ab76907020549f03db1bbd4bc562c28dc2
严俊东 <1172482914@qq.com>
2019-03-30 15:54:48 +0800

    环境动态配置、集合名称修改

--- src/main/java/com/yanjd/blog/models/Post.java
+++ src/main/java/com/yanjd/blog/models/Post.java
@@ -2,11 +2,13 @@ package com.yanjd.blog.models;
 
 import lombok.Data;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 @Data
+@Document("blog_post")
 public class Post {
     private ObjectId id;
     private String title;
