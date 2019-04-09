4a98c23c683e71cac7ec2b9e28b165067d8dad83
严俊东 <1172482914@qq.com>
2019-04-09 23:19:11 +0800

    登录接口

--- src/main/java/com/yanjd/blog/config/BrowerSecurityConfig.java
+++ src/main/java/com/yanjd/blog/config/BrowerSecurityConfig.java
@@ -1,39 +1,27 @@
 package com.yanjd.blog.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @EnableWebSecurity
 public class BrowerSecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() throws Exception {
-        // ensure the passwords are encoded properly
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-    }
-
-
     @Configuration
     public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
-//            http
-//                    .antMatcher("/api/**")
-//                    .authorizeRequests()
-//                    .anyRequest().hasRole("ADMIN")
-//                    .and()
-//                    .httpBasic();
             http
                     .authorizeRequests()        // 定义哪些URL需要被保护、哪些不需要被保护
                     .antMatchers(
@@ -44,8 +32,38 @@ public class BrowerSecurityConfig {
                     .authenticated()
                     .and()
                     .httpBasic()
-            ;
+                    .and()
+                    .formLogin()
+                    //指定登录页是"/login"
+                    .loginPage("/login_page")
+                    .successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                            httpServletResponse.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = httpServletResponse.getWriter();
+                            out.write("{\"status\":\"ok\",\"msg\":\"登录成功\"}");
+                            out.flush();
+                            out.close();
+                        }
+                    })
+                    .failureHandler(new AuthenticationFailureHandler() {
+                        @Override
+                        public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                            httpServletResponse.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = httpServletResponse.getWriter();
+                            out.write("{\"status\":\"error\",\"msg\":\"账号密码错误\"}");
+                            out.flush();
+                            out.close();
+                        }
+                    }).loginProcessingUrl("/login")
+                    //登录成功后默认跳转到
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .permitAll();
+
+            http.csrf().disable();
         }
     }
-
 }
